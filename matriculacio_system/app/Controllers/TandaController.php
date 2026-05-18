9bdcbf75ba114378fce1b5dcdd704ccb0939a8f9
Elbachir <elbachirzriguinat@gmail.com>
2026-05-18 18:30:37 +0200

    Controllers and models added

---
+++ matriculacio_system/app/Controllers/TandaController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Controllers\BaseController;
+use CodeIgniter\HTTP\ResponseInterface;
+
+class TandaController extends BaseController
+{
+    public function index()
+    {
+        //
+    }
+}
