dd4fec33cfc01ede29cc8e002fee7d054ada38f3
Elbachir <elbachirzriguinat@gmail.com>
2026-05-18 18:21:59 +0200

    Mailing file smtp added

--- matriculacio_system/app/Config/Email.php
+++ matriculacio_system/app/Config/Email.php
@@ -6,10 +6,10 @@ use CodeIgniter\Config\BaseConfig;
 
 class Email extends BaseConfig
 {
-    public string $fromEmail  = '';
-    public string $fromName   = '';
+    public string $fromEmail  = 'elbachirzriguinat@gmail.com';
+    public string $fromName   = 'Instituto Caparrella ';
     public string $recipients = '';
-
+    
     /**
      * The "user agent"
      */
@@ -18,7 +18,7 @@ class Email extends BaseConfig
     /**
      * The mail sending protocol: mail, sendmail, smtp
      */
-    public string $protocol = 'mail';
+    public string $protocol = 'smtp';
 
     /**
      * The server path to Sendmail.
@@ -28,32 +28,27 @@ class Email extends BaseConfig
     /**
      * SMTP Server Hostname
      */
-    public string $SMTPHost = '';
-
-    /**
-     * Which SMTP authentication method to use: login, plain
-     */
-    public string $SMTPAuthMethod = 'login';
+    public string $SMTPHost = 'smtp.gmail.com';
 
     /**
      * SMTP Username
      */
-    public string $SMTPUser = '';
-
+    public string $SMTPUser = 'elbachirzriguinat@gmail.com';
+    
     /**
      * SMTP Password
      */
-    public string $SMTPPass = '';
+    public string $SMTPPass = 'whcg jvfu gwuf hqdi';
 
     /**
      * SMTP Port
      */
-    public int $SMTPPort = 25;
+    public int $SMTPPort = 465;
 
     /**
      * SMTP Timeout (in seconds)
      */
-    public int $SMTPTimeout = 5;
+    public int $SMTPTimeout = 60;
 
     /**
      * Enable persistent SMTP connections
@@ -67,7 +62,9 @@ class Email extends BaseConfig
      *             to the server. 'ssl' means implicit SSL. Connection on port
      *             465 should set this to ''.
      */
-    public string $SMTPCrypto = 'tls';
+    public string $SMTPCrypto = 'ssl';
+    
+    
 
     /**
      * Enable word-wrap
@@ -82,7 +79,7 @@ class Email extends BaseConfig
     /**
      * Type of mail, either 'text' or 'html'
      */
-    public string $mailType = 'text';
+    public string $mailType = 'html';
 
     /**
      * Character set (utf-8, iso-8859-1, etc.)
@@ -117,6 +114,7 @@ class Email extends BaseConfig
     /**
      * Number of emails in each BCC batch
      */
+
     public int $BCCBatchSize = 200;
 
     /**
