57f348a924e4cf6a098ed7696f9b1749a40e9fa5
Elbachir <elbachirzriguinat@gmail.com>
2026-05-19 13:33:34 +0200

    data added to matricula pago

--- matriculacio_system/app/Views/Public/matricula/matricula_pago.php
+++ matriculacio_system/app/Views/Public/matricula/matricula_pago.php
@@ -8,7 +8,36 @@
 <body>
     <h1>pagos de la matricula 
 
-    
+     <div class="card mb-4 border-0 bg-light">
+
+                <div class="card-body">
+
+                    <h5 class="mb-3">
+                        Resumen de matrícula
+                    </h5>
+
+                    <ul class="list-group">
+
+                        <li class="list-group-item d-flex justify-content-between">
+                            <span>Alumno</span>
+                            <strong><?= esc($alumne['Nom_alumne']) ?></strong>
+                        </li>
+
+                        <li class="list-group-item d-flex justify-content-between">
+                            <span>Email</span>
+                            <strong><?= esc($alumne['correo_alumne']) ?></strong>
+                        </li>
+
+                        <li class="list-group-item d-flex justify-content-between">
+                            <span>Curso</span>
+                            <strong><?= esc($curs['Nom_curs']) ?></strong>
+                        </li>
+
+                        <li class="list-group-item d-flex justify-content-between">
+                            <span>Código curso</span>
+                            <strong><?= esc($curs['codigo_curs']) ?></strong>
+                        </li>
+
     </h1>
 </body>
 </html>
